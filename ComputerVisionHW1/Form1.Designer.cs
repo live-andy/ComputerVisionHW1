5d34c94f52ef32a944297286bd047fdd0cf2114d
live-andy <andy410061@gmail.com>
2015-09-26 15:41:12 +0800

    Good!

--- ComputerVisionHW1/Form1.Designer.cs
+++ ComputerVisionHW1/Form1.Designer.cs
@@ -39,6 +39,10 @@
             this.RotateVariable = new System.Windows.Forms.TextBox();
             this.RotateVariableLable = new System.Windows.Forms.Label();
             this.ProcessButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.MainPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             this.SuspendLayout();
@@ -82,7 +86,7 @@
             // 
             this.WidthVariableLabel.AutoSize = true;
             this.WidthVariableLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WidthVariableLabel.Location = new System.Drawing.Point(12, 136);
+            this.WidthVariableLabel.Location = new System.Drawing.Point(12, 209);
             this.WidthVariableLabel.Name = "WidthVariableLabel";
             this.WidthVariableLabel.Size = new System.Drawing.Size(102, 25);
             this.WidthVariableLabel.TabIndex = 3;
@@ -90,7 +94,7 @@
             // 
             // WidthVariable
             // 
-            this.WidthVariable.Location = new System.Drawing.Point(120, 142);
+            this.WidthVariable.Location = new System.Drawing.Point(120, 215);
             this.WidthVariable.Name = "WidthVariable";
             this.WidthVariable.Size = new System.Drawing.Size(56, 20);
             this.WidthVariable.TabIndex = 4;
@@ -99,7 +103,7 @@
             // 
             this.HeighVariableLabel.AutoSize = true;
             this.HeighVariableLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HeighVariableLabel.Location = new System.Drawing.Point(12, 178);
+            this.HeighVariableLabel.Location = new System.Drawing.Point(12, 251);
             this.HeighVariableLabel.Name = "HeighVariableLabel";
             this.HeighVariableLabel.Size = new System.Drawing.Size(102, 25);
             this.HeighVariableLabel.TabIndex = 5;
@@ -107,14 +111,14 @@
             // 
             // HeighVariable
             // 
-            this.HeighVariable.Location = new System.Drawing.Point(120, 184);
+            this.HeighVariable.Location = new System.Drawing.Point(120, 257);
             this.HeighVariable.Name = "HeighVariable";
             this.HeighVariable.Size = new System.Drawing.Size(56, 20);
             this.HeighVariable.TabIndex = 6;
             // 
             // RotateVariable
             // 
-            this.RotateVariable.Location = new System.Drawing.Point(120, 226);
+            this.RotateVariable.Location = new System.Drawing.Point(120, 299);
             this.RotateVariable.Name = "RotateVariable";
             this.RotateVariable.Size = new System.Drawing.Size(56, 20);
             this.RotateVariable.TabIndex = 7;
@@ -123,7 +127,7 @@
             // 
             this.RotateVariableLable.AutoSize = true;
             this.RotateVariableLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RotateVariableLable.Location = new System.Drawing.Point(12, 220);
+            this.RotateVariableLable.Location = new System.Drawing.Point(12, 293);
             this.RotateVariableLable.Name = "RotateVariableLable";
             this.RotateVariableLable.Size = new System.Drawing.Size(102, 25);
             this.RotateVariableLable.TabIndex = 8;
@@ -132,18 +136,56 @@
             // ProcessButton
             // 
             this.ProcessButton.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ProcessButton.Location = new System.Drawing.Point(13, 261);
+            this.ProcessButton.Location = new System.Drawing.Point(13, 334);
             this.ProcessButton.Name = "ProcessButton";
             this.ProcessButton.Size = new System.Drawing.Size(163, 46);
             this.ProcessButton.TabIndex = 9;
             this.ProcessButton.Text = "開始處理";
             this.ProcessButton.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "橫向平移:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "縱向平移:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(121, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(121, 178);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(912, 445);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.ProcessButton);
             this.Controls.Add(this.RotateVariableLable);
             this.Controls.Add(this.RotateVariable);
@@ -177,6 +219,10 @@
         private System.Windows.Forms.Label RotateVariableLable;
         private System.Windows.Forms.TextBox RotateVariable;
         private System.Windows.Forms.Button ProcessButton;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
